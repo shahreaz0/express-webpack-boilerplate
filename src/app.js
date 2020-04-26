19900ee5444da46e40591cc75d5ddc854778cffe
shahreaz0 <ashahreaz@gmail.com>
2020-04-27 01:07:13 +0600

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//modules
+const path = require("path");
+const express = require("express");
+//Router()
+const homeRouter = require("./routes/home");
+
+//express config
+const app = express();
+//set
+app.set("views", path.join(__dirname, "./../views"));
+app.set("view engine", "ejs");
+//use
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "./../public")));
+
+//routes
+app.use("/", homeRouter);
+
+//exports
+module.exports = app;
