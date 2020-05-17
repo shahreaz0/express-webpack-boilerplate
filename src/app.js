c7d3b337d8b6507dd8c3831a9cb7828ef9498dd2
shahreaz0 <ashahreaz@gmail.com>
2020-05-18 01:26:12 +0600

    Some changes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,18 @@
 const path = require("path");
 const express = require("express");
 require("dotenv").config();
-console.log(process.env.MY_VAR);
 //Router()
 const homeRouter = require("./routes/home");
 
 //express config
 const app = express();
 //set
-app.set("views", path.join(__dirname, "./../views"));
+app.set("views", path.join("views"));
 app.set("view engine", "ejs");
 //use
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "./../public")));
+app.use(express.static(path.join("public")));
 
 //routes
 app.use("/", homeRouter);
